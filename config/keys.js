cfa3a3a072b8cf2f3770ebaa293b13c314eabc25
Gary Scopel <Gary@Garys-MacBook-Air.local>
2019-04-20 11:19:33 -0400

    User login scaffold and routing.

---
+++ config/keys.js
@@ -0,0 +1,3 @@
+module.exports = {
+  MongoURI: 'mongodb+srv://gscopel:Beefsteak21!@dapp-cluster-ilpve.mongodb.net/test?retryWrites=true'
+}
--- routes/user.js
+++ routes/user.js
@@ -2,10 +2,17 @@ const express = require('express');
 const router = express.Router();
 
 //Login Page
-router.get('/login', (req, res) => res.send('LOGIN TEST'));
+router.get('/login', (req, res) => res.render('login'));
 
 //Register Page
-router.get('/register', (req, res) => res.send('REGISTER TEST'));
+router.get('/register', (req, res) => res.render('register'));
+
+//Register Handle
+router.post('/register', (req, res) => {
+  console.log(req.body);
+  res.send('Hello');
+});
+
 
 
 module.exports = router;
